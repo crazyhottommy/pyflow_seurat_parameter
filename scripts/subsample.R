117e202e0f490f9faab0f30b9f47e2743717bc51
Scott Van Buren <skvanburen@gmail.com>
2021-04-25 13:32:20 -0400

    Modify for Seurat v4 and add "useSCTransform" function arguments

--- scripts/subsample.R
+++ scripts/subsample.R
@@ -40,7 +40,14 @@ PreprocessSubsetData<- function(object,
                                 nn.eps = 0,
                                 resolution = 0.8,
                                 k.param = 30,
+                                useSCTransform = TRUE,
                                 ...){
+    if(!is.null(pc.use)){
+        if(pc.use > num.pc){
+            stop("Specify the maximum pc.use number as less than or equal to the total num.pc")
+        }
+    }
+
         ## use future for parallelization
         ## future::plan("multiprocess", workers = workers)
         meta.data.colnames<- object@meta.data %>% colnames()
@@ -49,22 +56,33 @@ PreprocessSubsetData<- function(object,
         vars.to.regress<- vars.to.regress[vars.to.regress %in% meta.data.colnames]
         # default is on variable features only, omit the features argument
         # SCTransform replaces NormalizeData, ScaleData and FindVariableFeatures
-        object<- SCTransform(object, vars.to.regress = vars.to.regress,
-                             variable.features.n = variable.features.n, verbose = FALSE)
+
+        if(useSCTransform==TRUE){
+            object<- SCTransform(object, vars.to.regress = vars.to.regress,
+                                 variable.features.n = variable.features.n, verbose = FALSE)  
+        }else{
+            stop("The SCTransform method for normalization is the only method currently supported by this function.  If you wish to use the approach that involves NormalizeData, ScaleData, and FindVariableFeatures and enables use of the Jackstraw procedure for determining which PCs to use please use the PreprocessSubsetDataV2 function from the scclusteval R package.")
+            
+        }
 
         object<- RunPCA(object = object, features = VariableFeatures(object = object),
                         npcs = num.pc)
 
-        if (is.null(pc.use)){
-                object<- JackStraw( object = object, num.replicate = 100, dims = num.pc)
-
-                object <- ScoreJackStraw(object = object, dims = 1:num.pc, score.thresh = score.thresh)
-
-                PC_pvalues<- object@reductions$pca@jackstraw@overall.p.values
-
-                ## determin how many PCs to use.
-                pc.use<- min(which(PC_pvalues[,"Score"] > sig.pc.thresh)) -1
-
+        # if (is.null(pc.use)){
+        #         object<- JackStraw( object = object, num.replicate = 100, dims = num.pc)
+        # 
+        #         object <- ScoreJackStraw(object = object, dims = 1:num.pc, score.thresh = score.thresh)
+        # 
+        #         PC_pvalues<- object@reductions$pca@jackstraw@overall.p.values
+        # 
+        #         ## determin how many PCs to use.
+        #         pc.use<- min(which(PC_pvalues[,"Score"] > sig.pc.thresh)) -1
+        # 
+        # }
+        
+        if(is.null(pc.use)){
+            pc.use <- num.pc
+            message("SCTransform is being used and the Jackstraw procedure for determining which PCs to use is not compatable with this procedure. Since pc.use was not specified it is being automatically set to num.pc")
         }
 
         # add significant pc number to metadata, need to have names same as the cells
@@ -73,7 +91,7 @@ PreprocessSubsetData<- function(object,
         object<- AddMetaData(object = object, metadata = pc.use.meta, col.name = "pc.use")
         object<- FindNeighbors(object, dims = 1:pc.use, k.param = k.param, nn.eps = nn.eps,
                                verbose = FALSE, reduction = "pca", force.recalc = TRUE)
-        object <- FindClusters(object = object, reduction = "pca",
+        object <- FindClusters(object = object,
                                 n.start = n.start,
                                 resolution = resolution,
                                 verbose = FALSE)
